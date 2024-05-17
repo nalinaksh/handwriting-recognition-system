8c005e053de2aa91d537ecd8dab22abdf40cd441
Nalinaksh Gaur <nalinaksh@gmail.com>
2024-05-17 17:00:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,6 +65,6 @@ if uploaded_file is not None:
   image = Image.open(uploaded_file)
   st.image(image, caption="Uploaded Image")
   with st.spinner('Recognizing text ...'):
-    file_path = os.path.join("temp_dir", uploaded_file.name)
+    file_path = os.path.join(os.getcwd(), uploaded_file.name)
     story = image_to_text(file_path)
     st.write(story)
